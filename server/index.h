4782605db51b3b53dd68713271dee6c27ace7a44
lex232 <alexeikrivonosov@mail.ru>
2019-05-18 09:07:16 +0300

    Add files via upload

---
+++ server/index.h
@@ -0,0 +1,49 @@
+const char MAIN_page[] PROGMEM = R"=====(
+<HTML>
+	<HEAD>
+			<TITLE>Control page</TITLE>
+      <style type="text/css">
+p {color:#006633}
+a.green{
+border-radius: 4px;
+-webkit-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.08);
+-moz-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.08);
+box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.08);
+color: #fff;
+display:block;
+width:100px;
+text-align: center;
+font-family: Arial, Helvetica, sans-serif;
+font-size: 14px;
+padding: 8px 16px;
+margin: 20px auto;
+text-decoration: none;
+text-shadow: 0 1px 1px rgba(0, 0, 0, 0.075);
+-webkit-transition: background-color 0.1s linear;
+-moz-transition: background-color 0.1s linear;
+-o-transition: background-color 0.1s linear;
+transition: background-color 0.1s linear;        
+}
+a.green {
+background-color: rgb( 43, 153, 91 );
+border: 1px solid rgb( 33, 126, 74 );
+}     
+a.green:hover {
+background-color: rgb( 75, 183, 141 );
+}
+body {background-color:#FFFFFF};
+</style>
+	</HEAD>
+<BODY>
+	<CENTER>
+			<h2><B>Main page</B></h2>
+      <a href="#" class="green">Light</a>
+      <a href="#" class="green">Relay</a>
+      <a href="#" class="green">Door</a>
+	</CENTER>	
+</BODY>
+</HTML>
+)=====";
+
+
+
---
+++ server/server.ino
@@ -0,0 +1,52 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#import "index.h"
+
+/* Put your SSID & Password */
+const char* ssid = "iPhone128";  // Enter SSID here
+const char* password = "1234567890";  //Enter Password here
+
+//const char* ssid = "tfsi";  // Enter SSID here
+//const char* password = "peugeot406";  //Enter Password here
+char value = 87;
+
+IPAddress ip(192, 168, 2, 177);
+IPAddress gateway(192, 168, 2, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+ESP8266WebServer server(80);
+
+void setup() {
+  Serial.begin(9600);
+  delay(100);
+  WiFi.mode(WIFI_STA);
+
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+
+  //connect to your local wi-fi network
+  WiFi.begin(ssid, password);
+  WiFi.config(ip, gateway, subnet);
+
+  //check wi-fi is connected to wi-fi network
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected!");
+  Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
+
+  server.begin();
+  server.on("/", handleRoot);
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+}
+
+void handleRoot() {
+  String s = MAIN_page;
+  server.send(200, "text/html", s);
+}
