da0f777da7accc65253dc2ed58b19040ed7a56ed
lex232 <alexeikrivonosov@mail.ru>
2019-06-15 19:13:08 +0300

    Add files via upload

---
+++ graphic_esp8266/graphic_esp8266.ino
@@ -0,0 +1,153 @@
+/**************************************************************************
+  This is a library for several Adafruit displays based on ST77* drivers.
+
+  Works with the Adafruit 1.8" TFT Breakout w/SD card
+    ----> http://www.adafruit.com/products/358
+  The 1.8" TFT shield
+    ----> https://www.adafruit.com/product/802
+  The 1.44" TFT breakout
+    ----> https://www.adafruit.com/product/2088
+  as well as Adafruit raw 1.8" TFT display
+    ----> http://www.adafruit.com/products/618
+
+  Check out the links above for our tutorials and wiring diagrams.
+  These displays use SPI to communicate, 4 or 5 pins are required to
+  interface (RST is optional).
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ **************************************************************************/
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>
+#import "dog.h"
+#import "dog2.h"
+
+#define TFT_CS         D2
+#define TFT_RST        D3
+#define TFT_DC         D4
+//
+// define not needed for all pins; reference for ESP32 physical pins connections to VSPI:
+// SDA  GPIO23 aka VSPI MOSI
+// SCLK GPIO18 aka SCK aka VSPI SCK
+// D/C  GPIO21 aka A0 (also I2C SDA)
+// RST  GPIO22 aka RESET (also I2C SCL)
+// CS   GPIO5  aka chip select
+// LED  3.3V
+// VCC  5V
+// GND - GND
+//
+
+
+// OPTION 1 (recommended) is to use the HARDWARE SPI pins, which are unique
+// to each board and not reassignable. For Arduino Uno: MOSI = pin 11 and
+// SCLK = pin 13. This is the fastest mode of operation and is required if
+// using the breakout board's microSD card.
+
+// For 1.44" and 1.8" TFT with ST7735 (including HalloWing) use:
+//Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+
+// For 1.54" TFT with ST7789:
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+// OPTION 2 lets you interface the display using ANY TWO or THREE PINS,
+// tradeoff being that performance is not as fast as hardware SPI above.
+//#define TFT_MOSI 11  // Data out
+//#define TFT_SCLK 13  // Clock out
+//Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
+float p = 3.1415926;
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.print(F("Hello! ST77xx TFT Test"));
+
+#ifdef ADAFRUIT_HALLOWING
+  // HalloWing is a special case. It uses a ST7735R display just like the
+  // breakout board, but the orientation and backlight control are different.
+  tft.initR(INITR_HALLOWING);        // Initialize HalloWing-oriented screen
+  pinMode(TFT_BACKLIGHT, OUTPUT);
+  digitalWrite(TFT_BACKLIGHT, HIGH); // Backlight on
+#else
+  // Use this initializer if using a 1.8" TFT screen:
+  // tft.initR(INITR_BLACKTAB);      // Init ST7735S chip, black tab
+
+  // OR use this initializer (uncomment) if using a 1.44" TFT:
+  //tft.initR(INITR_144GREENTAB); // Init ST7735R chip, green tab
+
+  // OR use this initializer (uncomment) if using a 0.96" 180x60 TFT:
+  //tft.initR(INITR_MINI160x80);  // Init ST7735S mini display
+
+  // OR use this initializer (uncomment) if using a 1.54" 240x240 TFT:
+  tft.init(240, 240);           // Init ST7789 240x240
+#endif
+
+  Serial.println(F("Initialized"));
+
+  uint16_t time = millis();
+  tft.fillScreen(ST77XX_BLACK);
+  time = millis() - time;
+
+  Serial.println(time, DEC);
+  delay(500);
+
+}
+
+void loop(void) {
+  tft.drawRGBBitmap(0, 0, DogBitmap, 240, 240);
+  delay(5000);
+  tft.drawRGBBitmap(0, 0, Dog2Bitmap, 240, 240);
+  delay(5000);
+  tft.fillScreen(ST77XX_BLACK);
+  test();
+}
+
+void test() {
+  testfillcircles(10, ST77XX_BLUE);
+  testdrawcircles(10, ST77XX_WHITE);
+  delay(500);
+  tft.fillScreen(ST77XX_BLACK);
+  testtriangles();
+  delay(500);
+  tft.fillScreen(ST77XX_BLACK);
+}
+
+
+void testfillcircles(uint8_t radius, uint16_t color) {
+  for (int16_t x = radius; x < tft.width(); x += radius * 2) {
+    for (int16_t y = radius; y < tft.height(); y += radius * 2) {
+      tft.fillCircle(x, y, radius, color);
+    }
+  }
+}
+
+void testdrawcircles(uint8_t radius, uint16_t color) {
+  for (int16_t x = 0; x < tft.width() + radius; x += radius * 2) {
+    for (int16_t y = 0; y < tft.height() + radius; y += radius * 2) {
+      tft.drawCircle(x, y, radius, color);
+    }
+  }
+}
+
+void testtriangles() {
+  tft.fillScreen(ST77XX_BLACK);
+  int color = 0xF800;
+  int t;
+  int w = tft.width() / 2;
+  int x = tft.height() - 1;
+  int y = 0;
+  int z = tft.width();
+  for (t = 0 ; t <= 15; t++) {
+    tft.drawTriangle(w, y, y, x, z, x, color);
+    x -= 4;
+    y += 4;
+    z -= 4;
+    color += 100;
+  }
+}
